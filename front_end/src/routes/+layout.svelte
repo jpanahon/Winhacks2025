6a1bb7635d7c6cdb6772d9f39545c1635f99cc7f
jpanahon <36034467+jpanahon@users.noreply.github.com>
2025-02-15 13:54:20 -0500

    feat: fundamental homepage + leaderboards

--- front_end/src/routes/+layout.svelte
+++ front_end/src/routes/+layout.svelte
@@ -3,4 +3,23 @@
 	let { children } = $props();
 </script>
 
+<header>
+	<div id="profle-panel" style="float: right">
+		<p>
+			<img src="agent47.png" width="32px" height="32px" alt="profile picture" style="float: left; border-radius: 50%;">
+			Morose
+		</p>
+		<p>
+			Level 1
+		</p>
+	</div>
+</header>
 {@render children()}
+
+<style>
+	header {
+		width: 100%;
+		height: 5rem;
+		padding: 1rem;
+	}
+</style>
--- front_end/src/routes/+page.svelte
+++ front_end/src/routes/+page.svelte
@@ -1,2 +1,27 @@
-<h1>Hello world</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<section id="subjects">
+    <section class="subject">
+        <img src="English.jpg" alt="English">
+        <h1>English</h1>
+    </section>
+    <section class="subject">
+        <h1>Math</h1>
+    </section>
+    <section class="subject">
+        <h1>Computer Science</h1>
+    </section>
+</section>
+
+<style>
+    #subjects {
+        width: 50%;
+        margin: 0 auto;
+        vertical-align: middle;
+    }
+    .subject {
+        display: inline-block;
+        width: 25%;
+        padding: 1rem;
+        border: 1px solid green;
+        text-align: center;
+    }
+</style>
